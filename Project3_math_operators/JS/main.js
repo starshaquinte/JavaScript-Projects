aa341f5b172df3728d4f64b114e4ce3e1c32f976
starshaquinte <126527633+starshaquinte@users.noreply.github.com>
2023-03-22 22:07:26 -0400

    Add files via upload

---
+++ Project3_math_operators/JS/main.js
@@ -0,0 +1,26 @@
+function addition_Function()    {
+    var sum = 8 + 3; //the sum
+    document.getElementById("Math");
+    innerHTML = "The sum of  " + 8 + 3 + sum;
+}
+
+function subtraction_Function() {
+    var subtraction = 9 - 3;
+    document.getElementById("Math").innerHTML = "9 - 3 = " + subtraction;
+}
+
+function multiplication_Function()   {
+    var multiplication = 5 * 4;
+    document.getElementById("Math").innerHTML = "5 * 4 = " + simple_Math;
+}
+
+function division_Function()    {
+    var division = 67 / 8;
+    document.getElementById("Math").innerHTML = "67 / 8 = " +simple_Math;
+}
+
+function more_Math()    {
+    var simple_Math = (10 + 3) * 6 / 4 - 6;
+    document.getElementById("Math").innerHTML = "10 plus 3, multiplied by 6,
+    divided by 4 and then subtracted by 6 equals  " + simple_Math;
+}
