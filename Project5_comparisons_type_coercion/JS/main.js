2b64a282a9f6c7ba6b0f7eaacedb40b3290b9fe3
starshaquinte <126527633+starshaquinte@users.noreply.github.com>
2023-03-24 16:25:55 -0400

    Add files via upload

---
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,37 @@
+document.write(typeof 8);
+
+document.write("48" + 5);
+
+document.write(3E544);
+
+document.write(-2E138);
+
+document.write(55 > 34);
+
+document.write(24 < 23);
+
+console.log(6 * 3);
+
+console.log(12 < 10);
+
+document.write(7 == 7);
+
+document.write(4 == 29);
+
+x = 50;
+y = 50;
+document.write(x === y);
+
+A = yellow;
+David = 10;
+document.write(A === David);
+
+x = 99;
+34 = 99;
+document.write(x === 34);
+
+b = "I am happy";
+c = 25;
+document.write(b === c);
+
+
