298026bf3713987597dd2fac3acfeadb30c136a7
starshaquinte <126527633+starshaquinte@users.noreply.github.com>
2023-03-23 20:39:42 -0400

    Add files via upload

---
+++ Project4_dictionaries/JS/main.js
@@ -0,0 +1,11 @@
+function my_Dictionary()    {
+    var Person = {
+        firstName: "John",
+        lastName: "Jones",
+        age: 46,
+        gender: "male",
+        occupation: "Lawyer",
+        gender: "male"
+    };
+    document.getElementById("Dictionary").innerHTML = Person.gender;
+}
