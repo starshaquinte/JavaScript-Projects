fcb4c2aa16511d105b53b29110a81705881e9143
starshaquinte <126527633+starshaquinte@users.noreply.github.com>
2023-03-23 21:55:41 -0400

    Add files via upload

--- Project4_dictionaries/JS/main.js
+++ Project4_dictionaries/JS/main.js
@@ -1,11 +1,11 @@
-function my_Dictionary()    {
-    var Person = {
-        firstName: "John",
-        lastName: "Jones",
-        age: 46,
-        gender: "male",
-        occupation: "Lawyer",
-        gender: "male"
+function my_Dictionary()    { //Defined a function naming it my_Dictionary
+    var Person = {  //Defined a variable that is a person
+        firstName: "John", // Defined the key firstName with a value of John
+        lastName: "Jones", //Defined the key lastName with a value of Jones
+        age: 46, //Defined the key age with a value of 46
+        gender: "male", //defined a key gender with the value male
+        occupation: "Lawyer" //defined a key occupation with the value Lawyer
     };
-    document.getElementById("Dictionary").innerHTML = Person.gender;
+    delete Person.gender; //Used the delete operator to remove value male 
+    document.getElementById("Dictionary").innerHTML = Person.gender; //Put a value of male for the key gender
 }
