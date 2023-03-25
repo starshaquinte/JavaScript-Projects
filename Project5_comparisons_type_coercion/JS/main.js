7b64bfbc4af065785a2aee76a7538b357527ab93
starshaquinte <126527633+starshaquinte@users.noreply.github.com>
2023-03-24 21:41:08 -0400

    Add files via upload

--- Project5_comparisons_type_coercion/JS/main.js
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -1,37 +1,41 @@
-document.write(typeof 8);
+document.write(typeof 8); //defined type of operator of a variable of my choosing
 
-document.write("48" + 5);
+document.write("48" + 5); // utilized type coercion to convert different data types, string and a number 
 
-document.write(3E544);
+document.write(3E544); // utilized document write method to display infinity number
 
-document.write(-2E138);
+document.write(-2E138); //utilized document write method to display negative infinity number
 
-document.write(55 > 34);
+document.write(55 > 34); //utilized boolean logic to display true statement
 
-document.write(24 < 23);
+document.write(24 < 23); //utilized boolean logic to display false statement
 
-console.log(6 * 3);
+console.log(6 * 3); //performed math operation with cosole.log operation
 
-console.log(12 < 10);
+console.log(12 < 10); //utilized console.log to display true in browser
 
-document.write(7 == 7);
+document.write(7 == 7);  // utilized double equal sign operator to compare equal numbers are true
 
-document.write(4 == 29);
+document.write(4 == 29); // utilized double equal sign operator to compare numbers are not equal and false
 
 x = 50;
 y = 50;
-document.write(x === y);
+document.write(x === y); //utilized triplie equal sign operator to show comparison should be made
 
-A = yellow;
-David = 10;
-document.write(A === David);
 
-x = 99;
-34 = 99;
-document.write(x === 34);
+document.write(8 > 15 && 25 > 23); //utilzed AND operator to determine logic between operators 
 
-b = "I am happy";
-c = 25;
-document.write(b === c);
+document.write(42 < 76 && 99 < 43); //utilzed AND operator to determine logic between operators 
 
+document.write(66 > 23 || 18 > 78); //utilzed OR operator to determine logic between operators 
+
+document.write(86 > 167 || 52 > 422); //utilzed OR operator to determine logic between operators 
+
+function not_Function() {
+    document.getElementById("Not"). innerHTML = !(17 > 4); //utilized boolean NOT operator to determine logic between operators 
+}
+
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(345 > 456); //utilized boolean NOT operator to determine logic between operators 
+}
 
