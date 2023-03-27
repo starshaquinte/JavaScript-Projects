4bf8396c43d448d9447896b74c07dbc644500e64
starshaquinte <126527633+starshaquinte@users.noreply.github.com>
2023-03-27 18:41:34 -0400

    Add files via upload

--- Project6_ternary__operators_constructors/JS/keywords.js
+++ Project6_ternary__operators_constructors/JS/keywords.js
@@ -1,14 +1,14 @@
-function Vechicle(Make, Model, Year, Color) {
-    this.Vechicle_Make = Make;
-    this.Vechicle_Model = Model;
-    this.Vechicle_Year = Year;
-    this.Vechicle_Color = Color;
+function Vehicle(Make, Model, Year, Color) {   //wrote the vehicle function as an object constructor
+    this.Vehicle_Make = Make; //assigned keyword this to vehicle's make
+    this.Vehicle_Model = Model; //assigned keyword this to vehicle's model
+    this.Vehicle_Year = Year;   //assigned keyword this to vehicle's year
+    this.Vehicle_Color = Color; //assigned keyword this to vehicle's color
 }
-var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
-var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
-var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red"); //created vehicle object for variable Jack
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black"); //created vehicle object for variable Emily
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard"); //created vehicle object for variable Erik
 function myFunction()   {
-    document.getElementById("Keywords_and_Constructors").innerHTML = 
+    document.getElementById("Keywords_and_Constructors").innerHTML = //Utilized method to return value
     "Erik drives a " + Erik.Vechicle_Color + "-colored" + Erik.Vehicle_Model +
     " manufactured in " + Erik.Vechicle_Year;
 }
--- Project6_ternary__operators_constructors/JS/main.js
+++ Project6_ternary__operators_constructors/JS/main.js
@@ -1,15 +1,25 @@
-function Ride_Function()    {
-    var Height, Can_ride;
-    Height = document.getElementById("Height").value;
-    Can_ride = (Height < 52) ? "You are too short": "You are tall enough";
-    document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
+function Ride_Function()    { //Declared ride function
+    var Height, Can_ride; //variable  height and can_ride
+    Height = document.getElementById("Height").value; //utilized get element by id method
+    Can_ride = (Height < 52) ? "You are too short": "You are tall enough"; //Declared height needed to ride
+    document.getElementById("Ride").innerHTML = Can_ride + " to ride."; //Utilzed method to show results
 }
 
 function Ride_Function(Height)  {
     this.Height = Can_ride;
 }
-var Lisa = new Height(53);
-function myFunction()   {
-    document.getElementById("Keywords_and_Constructors").innerHTML = 
-    "Lisa is" + Lisa.Height + "inches tall";
+var Lisa = new Height(53); //variable lisa and declare height
+function myFunction()   { //declared my function
+    document.getElementById("Keywords_and_Constructors").innerHTML = //utilized get element by id method
+    "Lisa is" + Lisa.Height + "inches tall"; //return of how tall Lisa is 
+}
+
+function count_Function()   { //Declared count function
+    document.getElementById("Counting").innerHTML = Count(); //utilized get element by id method
+    function Count()    { // declared count function continued 
+        var Starting_point = 40; //declared starting point variable
+        function Plus_one() {Starting_point += 2;} //utilize nested function to add 2
+        Plus_two();
+        return Starting_point; //Utilized return command to exit function
+    }
 }
