d32dc3f5d341893445125547fc16ee48885401af
starshaquinte <126527633+starshaquinte@users.noreply.github.com>
2023-03-27 20:27:12 -0400

    Add files via upload

--- Project6_ternary__operators_constructors/JS/main.js
+++ Project6_ternary__operators_constructors/JS/main.js
@@ -5,21 +5,12 @@ function Ride_Function()    { //Declared ride function
     document.getElementById("Ride").innerHTML = Can_ride + " to ride."; //Utilzed method to show results
 }
 
-function Ride_Function(Height)  {
-    this.Height = Can_ride;
-}
-var Lisa = new Height(53); //variable lisa and declare height
-function myFunction()   { //declared my function
-    document.getElementById("Keywords_and_Constructors").innerHTML = //utilized get element by id method
-    "Lisa is" + Lisa.Height + "inches tall"; //return of how tall Lisa is 
-}
-
 function count_Function()   { //Declared count function
     document.getElementById("Counting").innerHTML = Count(); //utilized get element by id method
     function Count()    { // declared count function continued 
         var Starting_point = 40; //declared starting point variable
-        function Plus_one() {Starting_point += 2;} //utilize nested function to add 2
-        Plus_two();
+        function Plus_one() {Starting_point += 1;} //utilize nested function to add 1
+        Plus_one();
         return Starting_point; //Utilized return command to exit function
     }
 }
