2bb6685d3d8a1387cdb60f16a809fafc37d8f92b
starshaquinte <126527633+starshaquinte@users.noreply.github.com>
2023-03-28 20:49:53 -0400

    Add files via upload

---
+++ Project8_string_methods/JS/main.js
@@ -0,0 +1,27 @@
+function full_sentence()    { //wrote function utilizing the concat() method 
+    var part_1 = " I love ";
+    var part_2 = " the colors ";
+    var part_3 = " blue ";
+    var part_4 = " purple ";
+    var part_5 = " and green. ";
+    var whole_sentence = part_1.concat(part_2, part_3, part_4, part_5);
+    document.getElementById("Concatente").innerHTML = whole_sentence;
+}
+
+function slice_Method() {   //wrote function utilizing the slice() method 
+    var Sentence = "I love to eat tacos on Tuesday.";
+    var section = Sentence.slice(14,18);
+    document.getElementById("Slice").innerHTML = Section;
+}
+
+function toString() {   //wrote function utilizing the toString() method 
+    x = 25;
+    document.getElementById("toString").innerHTML = x.toString();
+    (25).toString();
+}
+
+function toPrecision()  {   //wrote function utilizing the toPrecision method 
+    x = 6.885;
+    document.getElementById("toPrecision").innerHTML = x.toPrecision(2);
+}
+
