56032ed29c8b36e9be356dd5984bedc62d295e00
starshaquinte <126527633+starshaquinte@users.noreply.github.com>
2023-03-29 15:06:53 -0400

    Add files via upload

---
+++ Project9_countdown_slideshow/JS/main.js
@@ -0,0 +1,42 @@
+function countdown()    {
+    var seconds = document.getElementById("seconds").value;
+
+    function tick() {   // Function to compute seconds on countdown
+        seconds = seconds - 1;
+        timer.innerHTML = seconds;
+        var time = setTimeout(tick, 1000);
+        if (seconds == -1)  {
+            alert("Time's up!");
+            clearTimeout(time);
+            timer.innerHTML = "";
+        }
+    }
+    tick();
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {    // Next/previous
+    showSlides(slideIndex += n);
+  }
+
+function currentSlide(n) {  // Thumbnail image controls
+    showSlides(slideIndex = n);
+  }
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+  }
